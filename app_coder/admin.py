0c10ed6ed275a14675345770e5f826eda3ba2156
Fernanda <fernandaselmen@gmail.com>
2023-03-28 18:08:45 -0300

    segundo formulario

--- app_coder/admin.py
+++ app_coder/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from app_coder.models import Curso
+from app_coder.models import Curso, Alumno
 # Register your models here.
+
+
 admin.site.register(Curso)
+
+
+admin.site.register(Alumno)
--- app_coder/urls.py
+++ app_coder/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from app_coder.views import inicio , bucadorCurso
+from app_coder.views import inicio , bucadorCurso, bucadorAlumno
 urlpatterns = [
     path('index/', inicio, name = 'inicio' ),
-    path('buscador_curso/', bucadorCurso, name = 'curso' ),
+    path('buscador_curso/', bucadorCurso, name = 'Curso' ),
+
+    path('buscador_alumno/', bucadorAlumno, name = 'Alumno' ),
 ]
