b98fee209cee6f1a446dc791f18ca33c49185929
Fernanda <fernandaselmen@gmail.com>
2023-03-28 16:25:37 -0300

    creacion de formulario

--- app_coder/views.py
+++ app_coder/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+from app_coder.models import Curso
 # Create your views here.
 
 def inicio(request):
     return render(request, 'app_coder/index.html')
+
+
+def bucadorCurso( request):
+     
+     if request.method == 'POST':
+      
+            curso =  Curso(request.post['curso'],(request.post['camada']))
+ 
+            curso.save()
+
+            return render(request, 'app_coder/buscador_curso.html')
+
+     return render(request, 'app_coder/buscador_curso.html')
+
